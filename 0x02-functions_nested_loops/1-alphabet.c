3fc654518f50afd0e7bff8e56a7bc03582311e6b
VanessaNjoroge <vanessa.n.njoroge@gmail.com>
2023-02-21 12:36:35 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+	/**
+	 * main-print_alphabet.c
+	 * print_alphabet - print all alphabet in lowercase
+	 * Return: void
+	 */
+
+
+	void print_alphabet(void)
+	{
+		char letter;
+
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+
+
+		_putchar('\n');
+	}
+
